7970aebaaf9f4a28947eb89d81594f6825ebb28c
BallyMcGee <mtdab007@learner.dvusd.org>
2024-09-20 12:38:57 -0700

    first version of game

---
+++ Tetris/Tetris/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Avatar = New PictureBox()
+        Timer1 = New Timer(components)
+        Banana = New PictureBox()
+        Banana2 = New PictureBox()
+        Enemy = New PictureBox()
+        Enemy2 = New PictureBox()
+        CType(Avatar, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Banana, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Banana2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Enemy, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Enemy2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Avatar
+        ' 
+        Avatar.Image = CType(resources.GetObject("Avatar.Image"), Image)
+        Avatar.Location = New Point(241, 31)
+        Avatar.Name = "Avatar"
+        Avatar.Size = New Size(262, 157)
+        Avatar.SizeMode = PictureBoxSizeMode.StretchImage
+        Avatar.TabIndex = 0
+        Avatar.TabStop = False
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 500
+        ' 
+        ' Banana
+        ' 
+        Banana.Image = My.Resources.Resources.Banana
+        Banana.Location = New Point(296, 234)
+        Banana.Name = "Banana"
+        Banana.Size = New Size(225, 114)
+        Banana.SizeMode = PictureBoxSizeMode.StretchImage
+        Banana.TabIndex = 2
+        Banana.TabStop = False
+        Banana.Tag = "Banana"
+        ' 
+        ' Banana2
+        ' 
+        Banana2.Image = My.Resources.Resources.Banana
+        Banana2.Location = New Point(551, 234)
+        Banana2.Name = "Banana2"
+        Banana2.Size = New Size(228, 113)
+        Banana2.SizeMode = PictureBoxSizeMode.StretchImage
+        Banana2.TabIndex = 3
+        Banana2.TabStop = False
+        Banana2.Tag = "Banana"
+        ' 
+        ' Enemy
+        ' 
+        Enemy.Image = CType(resources.GetObject("Enemy.Image"), Image)
+        Enemy.Location = New Point(60, 234)
+        Enemy.Name = "Enemy"
+        Enemy.Size = New Size(198, 126)
+        Enemy.SizeMode = PictureBoxSizeMode.StretchImage
+        Enemy.TabIndex = 4
+        Enemy.TabStop = False
+        ' 
+        ' Enemy2
+        ' 
+        Enemy2.Image = CType(resources.GetObject("Enemy2.Image"), Image)
+        Enemy2.Location = New Point(516, 261)
+        Enemy2.Name = "Enemy2"
+        Enemy2.Size = New Size(198, 126)
+        Enemy2.SizeMode = PictureBoxSizeMode.StretchImage
+        Enemy2.TabIndex = 5
+        Enemy2.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Enemy2)
+        Controls.Add(Enemy)
+        Controls.Add(Banana2)
+        Controls.Add(Banana)
+        Controls.Add(Avatar)
+        Name = "Form1"
+        Text = "Form1"
+        CType(Avatar, ComponentModel.ISupportInitialize).EndInit()
+        CType(Banana, ComponentModel.ISupportInitialize).EndInit()
+        CType(Banana2, ComponentModel.ISupportInitialize).EndInit()
+        CType(Enemy, ComponentModel.ISupportInitialize).EndInit()
+        CType(Enemy2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Avatar As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Banana As PictureBox
+    Friend WithEvents Banana2 As PictureBox
+    Friend WithEvents Enemy As PictureBox
+    Friend WithEvents Enemy2 As PictureBox
+
+End Class
---
+++ Tetris/Tetris/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        follow(Enemy, Avatar, 10)
+        follow(Enemy2, Enemy, 5)
+    End Sub
+    Sub follow(e As PictureBox, a As PictureBox, s As Integer)
+        If e.Location.Y > a.Location.Y Then
+            move(e, 0, -s)
+        Else
+            move(e, 0, s)
+        End If
+        If e.Location.X > a.Location.X Then
+            move(e, -s, 0)
+        Else
+            move(e, s, 0)
+        End If
+    End Sub
+
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.A Or e.KeyCode = Keys.Left Then
+            move(Avatar, -5, 0)
+        End If
+        If e.KeyCode = Keys.D Or e.KeyCode = Keys.Right Then
+            move(Avatar, 5, 0)
+
+        End If
+        If e.KeyCode = Keys.S Or e.KeyCode = Keys.Down Then
+            move(Avatar, 0, 15)
+        End If
+        If e.KeyCode = Keys.R Or e.KeyCode = Keys.Space Then
+            Avatar.Image.RotateFlip(RotateFlipType.Rotate90FlipXY)
+        End If
+        If e.KeyCode = Keys.W Or e.KeyCode = Keys.Up Then
+            move(Avatar, 0, -5)
+        End If
+        Avatar.Refresh()
+    End Sub
+    Sub move(p As PictureBox, xdir As Integer, ydir As Integer)
+        p.Location += New Point(xdir, ydir)
+        If p.Bounds.IntersectsWith(Banana.Bounds) Then
+            Banana.Visible = False
+        End If
+    End Sub
+End Class
